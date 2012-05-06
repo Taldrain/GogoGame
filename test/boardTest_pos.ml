e1451aeccdf39b3434790283ddc4f1691e8bad7b
Thomas Wickham <mackwic@gmail.com>
2012-05-07 01:42:10 +0800

    ajout des tests de la classe board

---
+++ test/boardTest_pos.ml
@@ -0,0 +1,54 @@
+(**
+De petits tests concernant la classe board
+**)
+open Common
+open OUnit
+    
+let default_size = 13
+  
+let r = BatRandom.self_init ()
+  
+let fill_board i =
+  let b = new Board.board default_size in
+  let next_col =
+    function
+    | Entities.Color.Black -> Entities.Color.White
+    | _ -> Entities.Color.Black
+  and random_move col =
+    {
+      Entities.Move.color = col;
+      Entities.Move.vert =
+        {
+          Entities.Vertex.letter = letter_of_int (BatRandom.int default_size);
+          Entities.Vertex.nb = BatRandom.int default_size;
+          Entities.Vertex.pass = false
+        };
+    } in
+  let rec fill i col =
+    if i = 0
+    then b
+    else (b#place_stone (random_move col); fill (i - 1) (next_col col))
+  in fill i Entities.Color.Black
+  
+let test_fill_board () = fill_board (default_size * default_size)
+  
+let empty_board b = (b#clear; b#is_clear)
+  
+let get_all b =
+  let dummy_ref = ref (Board.Corner (0, Entities.Color.Empty, (0, 0)))
+  in
+    (for i = 0 to (default_size * default_size - 1) do dummy_ref := b#get i done;
+     b)
+  
+let suite () =
+  "Board class" >:::
+    [ "remplissage" >::
+        (fun () -> "exception lors du remplissage" @? (test_fill_board (); true));
+      "vider" >::
+        (fun () -> "board n'a pas été vidée" @?
+           (bracket (fun () -> fill_board 30) empty_board (fun b -> b#is_clear) ()));
+      "get_all" >::
+        (fun () -> "impossible d'acceder a la totalite du plateau" @?
+           (bracket (fun () -> fill_board (default_size * default_size))
+              get_all (fun b -> true) ())) ]
+  
--- test/testing.ml
+++ test/testing.ml
@@ -1,17 +1,21 @@
 (**
 Tests de regression effectues a chaque build
 
-@see http://ounit.forge.ocamlcore.org/api/
+@see http:// ounit.forge.ocamlcore.org / api /
 **)
 
-(* Objectif : *)
-(*  - Deux suites de tests : *)
-(*      * tests rapides de regression, a chaque build*)
-(*      * tests longs et plus intensifs, sur des parties du code *)
-(*  - Tester beaucoup, mais pas tout non plus *)
+(* Objectif : - Deux suites de tests : * tests rapides de regression, a    *)
+(* chaque build * tests longs et plus intensifs, sur des parties du code - *)
+(* Tester beaucoup, mais pas tout non plus                                 *)
 
 open OUnit
 
+let main_suite = "main test suite" >:::
+  [
+  ParserTest_pos.suite ();
+  BoardTest_pos.suite ()
+  ]
+
 let _ =
-  run_test_tt_main (ParserTest_pos.suite ())
+  run_test_tt_main main_suite
 
